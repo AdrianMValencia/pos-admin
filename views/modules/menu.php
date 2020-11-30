f7c79297c953d543cbdc13daed0512f816189b74
Adrian Mechato <adrianmechatovalencia2@gmail.com>
2020-11-30 18:06:43 -0500

    POS | Implementación de plantilla AdminLTE | 30/11/2020

---
+++ views/modules/menu.php
@@ -0,0 +1,83 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+            <!--======================================
+                INICIO
+            =======================================-->
+            <li class="active">
+                <a href="">
+                    <i class="fa fa-home"></i>
+                    <span>Inicio</span>
+                </a>
+            </li>
+            <!--======================================
+                GESTION DE USUARIOS
+            =======================================-->
+            <li class="">
+                <a href="">
+                    <i class="fa fa-user"></i>
+                    <span>Usuarios</span>
+                </a>
+            </li>
+            <!--======================================
+                GESTION DE CATEGORIAS
+            =======================================-->
+            <li class="">
+                <a href="">
+                    <i class="fa fa-th"></i>
+                    <span>Categorías</span>
+                </a>
+            </li>
+            <!--======================================
+                GESTION DE PRODUCTOS
+            =======================================-->
+            <li class="">
+                <a href="">
+                    <i class="fa fa-product-hunt"></i>
+                    <span>Productos</span>
+                </a>
+            </li>
+            <!--======================================
+                GESTION DE CLIENTES
+            =======================================-->
+            <li class="">
+                <a href="">
+                    <i class="fa fa-users"></i>
+                    <span>Clientes</span>
+                </a>
+            </li>
+            <!--======================================
+                GESTION DE VENTAS
+            =======================================-->
+            <li class="treeview">
+                <a href="">
+                    <i class="fa fa-money"></i>
+                    <span>Ventas</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="">
+                            <i class="fa fa-tags"></i>
+                            <span>Administrar Ventas</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-cart-plus"></i>
+                            <span>Crear Venta</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-bar-chart-o "></i>
+                            <span>Reporte de Ventas</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+</aside>
